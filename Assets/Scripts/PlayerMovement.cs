326acb15c62b05654ae5e17fb1efcb90c76ab16e
Abdelrahman Mohamed <146343312+Abdelrahman-Mohamed-program@users.noreply.github.com>
2026-05-13 10:38:19 +0300

    Update PlayerMovement.cs

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,19 +8,36 @@ public class PlayerMovement : MonoBehaviour
 private bool isPaused = false;
     public float speed = 5f;
     private float velocityY;
-    private bool nearNPC = false;
+
 private bool inDialogue = false;
     private bool nearDoor = false;
 public float gravity = -9.81f;
     public float mouseSensitivity = 2f;
-
-
+private Animator animator;
+public Animator characterAnimator;
 
 public TextMeshProUGUI sleepText;
 
+//npc's
+private bool nearNPC = false;
+private bool dialogueActive = false;
+
+private string[] currentDialogue;
+private int dialogueIndex = 0;
+
+private string currentNPC;
 
 void OnTriggerEnter(Collider other)
 {
+    if (other.CompareTag("NPC"))
+{
+    nearNPC = true;
+
+    sleepText.text = "Press 1 to talk";
+    sleepText.gameObject.SetActive(true);
+
+    currentNPC = other.gameObject.name;
+}
     Debug.Log("TRIGGER HIT: " + other.name);
 
     if (other.CompareTag("Bed"))
@@ -32,15 +49,7 @@ void OnTriggerEnter(Collider other)
 
         Debug.Log("TEXT SHOULD SHOW NOW");
     }
-    if (other.CompareTag("NPC"))
-{
-    Debug.Log("NPC TRIGGER HIT");
-    nearNPC = true;
-
-    sleepText.text = "You are stronger than you think.\nPress E to continue";
-    sleepText.gameObject.SetActive(true);
-}
-
+   
     if (other.CompareTag("Door"))
     {
         nearDoor = true;
@@ -60,10 +69,14 @@ void OnTriggerExit(Collider other)
         nearBed = false;
         sleepText.gameObject.SetActive(false);
     }
-    if (other.CompareTag("NPC"))
+   if (other.CompareTag("NPC"))
 {
     nearNPC = false;
-    sleepText.gameObject.SetActive(false);
+
+    if (!dialogueActive)
+    {
+        sleepText.gameObject.SetActive(false);
+    }
 }
 
     if (other.CompareTag("Door"))
@@ -82,6 +95,7 @@ private bool nearBed = false;
 {
     controller = GetComponent<CharacterController>();
 
+animator = GetComponent<Animator>();
     if (sleepText == null)
         Debug.LogError("SleepText is NOT assigned in Inspector!");
 
@@ -131,29 +145,73 @@ void Sleep()
 
 void StartDialogue()
 {
-    inDialogue = true;
-
-    sleepText.text = "You are stronger than you think.\n\tkeep going\n\nPress E to continue";
+    dialogueActive = true;
 
     Time.timeScale = 0f;
 
-    Cursor.lockState = CursorLockMode.None;
-    Cursor.visible = true;
-}
+    if (currentNPC == "jordan")
+    {
+        currentDialogue = new string[]
+        {
+            "You don't have much time Son.",
+            "Stay focused.",
+            "Do not waste seconds."
+        };
+    }
+    else if (currentNPC == "remy")
+    {
+        currentDialogue = new string[]
+        {
+            "I still dance even in chaos.",
+            "Keeps the fear away."
+        };
+    }
+    else if (currentNPC == "bella")
+    {
+        currentDialogue = new string[]
+        {
+            "Something feels wrong here.",
+            "Be careful."
+        };
+    }
 
+    dialogueIndex = 0;
+
+    sleepText.text = currentDialogue[dialogueIndex];
+}
+void NextDialogue()
+{
+    dialogueIndex++;
+
+    if (dialogueIndex < currentDialogue.Length)
+    {
+        sleepText.text = currentDialogue[dialogueIndex];
+    }
+    else
+    {
+        EndDialogue();
+    }
+}
 void EndDialogue()
 {
-    inDialogue = false;
-
-    sleepText.gameObject.SetActive(false);
+    dialogueActive = false;
 
     Time.timeScale = 1f;
 
-    Cursor.lockState = CursorLockMode.Locked;
-    Cursor.visible = false;
+    sleepText.gameObject.SetActive(false);
 }
     void Update()
     {
+        if (nearNPC && !dialogueActive && Input.GetKeyDown(KeyCode.Alpha1))
+{
+    StartDialogue();
+}
+
+if (dialogueActive && Input.GetKeyDown(KeyCode.E))
+{
+    NextDialogue();
+}
+
     if (Input.GetKeyDown(KeyCode.Escape))
 {
     if (isPaused)
@@ -167,17 +225,6 @@ void EndDialogue()
 }
 
 
-if (nearNPC && Input.GetKeyDown(KeyCode.E))
-{
-    if (!inDialogue)
-    {
-        StartDialogue();
-    }
-    else
-    {
-        EndDialogue();
-    }
-}
 
         if (nearBed && Input.GetKeyDown(KeyCode.E)){
     Sleep();
@@ -197,7 +244,24 @@ void Move()
     if (inDialogue || isPaused) return;
     float x = Input.GetAxis("Horizontal");
     float z = Input.GetAxis("Vertical");
+bool isMoving = x != 0 || z != 0;
 
+
+if (isMoving)
+
+{
+
+    characterAnimator.speed = 1;
+
+}
+
+else
+
+{
+
+    characterAnimator.speed = 0;
+
+}
     Vector3 move = transform.right * x + transform.forward * z;
 
     // gravity handling
